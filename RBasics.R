b87369832baecf26d890cb2fb431f082501c45f0
ConnorCharchuk <ccharchu@ualberta.ca>
2017-12-04 17:41:38 -0700

    How to download data from GitHub

--- RBasics.R
+++ RBasics.R
@@ -1,3 +1,8 @@
 #Basics to R.
-B = 1
-C = 1
+#How to import your data from GitHub
+#Learning how to write functions and loops.
+library(RCurl)
+
+url <- "https://raw.githubusercontent.com/CCharchuk/BioRcoustics/master/Count_All_Birds_Each_Station_Crosstab.csv"
+sitespp <- getURL(url)
+sitespp <- read.csv(textConnection(sitespp))
