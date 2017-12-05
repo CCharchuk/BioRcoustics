1636cbd41f0b69cedeef4453b607629113e676fc
ConnorCharchuk <ccharchu@ualberta.ca>
2017-12-04 18:12:12 -0700

    Writing some basic code

--- RBasics.R
+++ RBasics.R
@@ -6,3 +6,12 @@ library(RCurl)
 url <- "https://raw.githubusercontent.com/CCharchuk/BioRcoustics/master/sitesppdata.csv"
 sitespp <- getURL(url)
 sitespp <- read.csv(textConnection(sitespp))
+
+sites <- as.factor(sitespp$SITE)
+stations <- sitespp$STATION
+richness <- sitespp$Total.Of.MaxOfINDIV_ID
+surveyyear <- sitespp$Year
+#Things work faster as a matrix 
+sitespp <- as.matrix(sitespp[,5:57])
+spp <- colnames(sitespp)
+
